ce989cd0bd27eea78a99fd3b41ee6a0c0a82aac6
igolubev <igolubevapp@gmail.com>
2020-09-07 13:57:52 +0300

    Encode example

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -37,6 +39,12 @@ func main() {
 }
 
 func foo(w http.ResponseWriter, r *http.Request) {
+	p1 := person{
+		First: "Jenny",
+	}
+	if err := json.NewEncoder(w).Encode(p1); err != nil {
+		log.Println("Encoded bad data", err)
+	}
 
 }
 
