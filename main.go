74441196b79c05bd2907671a3a75d7a4278b8dbe
igolubev <igolubevapp@gmail.com>
2020-09-07 14:31:29 +0300

    ninja 1 ex 1

--- main.go
+++ main.go
@@ -11,28 +11,6 @@ type person struct {
 }
 
 func main() {
-	// p1 := person{
-	// 	First: "Jenny",
-	// }
-
-	// p2 := person{
-	// 	First: "James",
-	// }
-
-	// xp := []person{p1, p2}
-
-	// bs, err := json.Marshal(xp)
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-	// fmt.Println("PRINT JSON", string(bs))
-
-	// xp2 := []person{}
-	// if err = json.Unmarshal(bs, &xp2); err != nil {
-	// 	log.Panic(err)
-	// }
-	// fmt.Println("back into Go data structure", xp2)
-
 	http.HandleFunc("/encode", foo)
 	http.HandleFunc("/decode", bar)
 	http.ListenAndServe(":8080", nil)
@@ -40,19 +18,20 @@ func main() {
 
 func foo(w http.ResponseWriter, r *http.Request) {
 	p1 := person{
-		First: "Jenny",
-	}
-	if err := json.NewEncoder(w).Encode(p1); err != nil {
-		log.Println("Encoded bad data", err)
+		First: "Janny",
 	}
 
+	p2 := person{
+		First: "James",
+	}
+
+	people := []person{p1, p2}
+
+	if err := json.NewEncoder(w).Encode(people); err != nil {
+		log.Println("Encode bad data", err)
+	}
 }
 
 func bar(w http.ResponseWriter, r *http.Request) {
-	var p1 person
-	if err := json.NewDecoder(r.Body).Decode(&p1); err != nil {
-		log.Println("Decoded bad data", err)
-	}
 
-	log.Println("Person:", p1)
 }
